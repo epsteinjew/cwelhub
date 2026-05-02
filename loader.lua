268cd58f2c10ae1b6e145d440f2cd8d23d0870e5
pps <pphitter@seznam.cz>
2026-05-02 02:28:42 +0200

    Update loader.lua

---
+++ loader.lua
@@ -0,0 +1,14 @@
+local gameId = game.PlaceId
+
+if gameId == 122363080663758 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/epsteinjew/cwelhub_games/refs/heads/main/122363080663758.lua"))()
+
+elseif gameId == 987654321 then
+    loadstring(game:HttpGet("https://pastebin.com/raw/script2"))()
+
+elseif gameId == 111111111 then
+    loadstring(game:HttpGet("https://pastebin.com/raw/script3"))()
+
+else
+    print("Unsupported game")
+end
